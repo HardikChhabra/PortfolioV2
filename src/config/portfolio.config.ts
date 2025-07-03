92757d72fd353c32ca6c84464536f846cbf80fde
Hardik <hardikchhabra3007@gmail.com>
2025-07-03 22:01:39 +0530

    Initial Commit

---
+++ src/config/portfolio.config.ts
@@ -0,0 +1,355 @@
+export const portfolioConfig = {
+  // Basic Information
+  name: "Hardik Chhabra",
+  title: "Software Engineer",
+  description: "I am a Passionate Software Developer",
+  location: "New Delhi, Delhi",
+
+  // Contact Information
+  email: "hardikchhabra3007@gmail.com", // Replace with actual email if available
+
+  // Social Media Links
+  //Fix no links error
+  socialLinks: {
+    /* facebook: "https://www.facebook.com/shahina.khatun.1044", */
+    twitter: "https://twitter.com/HardikChhabra30",
+    linkedin: "https://bit.ly/HardikChhabra-linkedin",
+    /* external: "https://tinyurl.com/MdTaquiImam", */
+    github: "https://github.com/hardikchhabra", // Assuming this is the GitHub link
+  },
+
+  // SEO Information
+  seo: {
+    ogImage: "https://mdtaquiimam.vercel.app/og-image.png",
+    url: "https://mdtaquiimam.vercel.app",
+    twitterHandle: "@Taquiimam14",
+    keywords: [
+      "md",
+      "taqui",
+      "imam",
+      "Md",
+      "MD",
+      "TAQUI",
+      "Taqui",
+      "Imam",
+      "Imam",
+      "Md Taqui Imam",
+      "md taqui imam",
+      "mdtaqui",
+      "mdtaquiimam",
+      "taqui imam",
+      "Taqui Imam",
+      "Taquiimam",
+      "portfolio",
+      "web developer",
+      "web",
+      "web dev",
+      "developer",
+      "PROGRAMMER ",
+      "programmer ",
+      "MD TAQUI IMAM ",
+      "website",
+      "@Taquiimam",
+      "Taquiimam",
+      "taqui developer",
+    ],
+    authors: [
+      {
+        name: "Hardik Chhabra",
+        url: "https://github.com/hardikchhabra",
+      },
+    ],
+  },
+
+  // About Information
+  about: {
+    bio: "A dedicated computer science student with a strong aptitude for problem-solving. My proficiency in algorithmic thinking and coding allows me to address challenges effectively and efficiently.",
+    hobbies: [
+      "Coding",
+      "Playing Games",
+      "Watching Anime",
+      "Creating Cool Projects",
+    ],
+    personalInfo: {
+      language: "English",
+      nationality: "India",
+      gender: "Male",
+    },
+  },
+
+  // Skills and Roles
+  skills: {
+    roles: [
+      "Creator",
+      "Student",
+      "Contributer",
+      "Programmer",
+      "Architect",
+      "Your Next Hire?",
+    ],
+    // Technical Skills
+    programmingLanguages: [
+      /* {
+        name: "HTML5",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      },
+      {
+        name: "CSS3",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+      }, */
+      {
+        name: "Java",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+      },
+      {
+        name: "JavaScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "TypeScript",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "C++",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "Python",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+      },
+      {
+        name: "Dart",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg",
+      },
+    ],
+    frameworks: [
+      {
+        name: "React",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      },
+      {
+        name: "Next.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      },
+      {
+        name: "Node.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+      },
+      {
+        name: "Flutter",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg",
+      },
+      /* {
+        name: "Django",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-original.svg",
+      }, */
+      {
+        name: "Flask",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg",
+      },
+      {
+        name: "Firebase",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-original.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+      },
+      {
+        name: "Supabase",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/supabase/supabase-original.svg",
+      },
+    ],
+    tools: [
+      {
+        name: "Git",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+      },
+      {
+        name: "GitLab",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg",
+      },
+      /* {
+        name: "Docker",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+      }, */
+      {
+        name: "MySQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+      },
+      /* {
+        name: "MongoDB",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      }, */
+      {
+        name: "PostgreSQL",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+      },
+      {
+        name: "Figma",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+      },
+    ],
+  },
+
+  // Education Information
+  education: [
+    {
+      degree: "Bachelor of Technology, Computer Science",
+      institution: "Indraprastha University",
+      location: "Vivekananda Institute of Professional Studies",
+      period: "July 2022 - 2026",
+      description:
+        "I am currently Studying Bachelor of Technology in Computer Science form Vivekanada Institute of Proffesional Studies, a college accredited under IPU. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
+    },
+  ],
+
+  //Experience Information
+  experience: [
+    {
+      degree: "Frontend Development Intern",
+      institution: "FoundIt (formerly Monster.com)",
+      location: "Noida",
+      period: "Jul 2024 - Oct 2024",
+      description:
+        "Enhanced frontend design through responsive techniques with Tailwind CSS and a modular component approach, allowing for efficient UI management and development and implemented profile page features (forms, modals) with React Hook Form and custom hooks, cutting data-fetch errors by 20% through API integration",
+    },
+  ],
+
+  // Projects Information
+  projects: [
+    /* {
+      title: "WePost- A modern, minimalistic social platform.",
+      description:
+        "WePost is a modern, minimalistic social platform for sharing posts, like, follow, and engaging in comments etc.",
+      tags: ["Tanstack-Query", "PostgreSQL", "Shadcn Ui", "Nextjs"],
+      link: "https://we-post-dev.vercel.app",
+    },
+    {
+      title: "Devzone- A community chat app for devs",
+      description:
+        "DevZone is the ultimate chat platform for developers to connect, collaborate, and code together. Join the conversation and level up your coding journey.",
+      tags: ["Zustand", "Supabase", "Shadcn Ui", "Nextjs"],
+      link: "https://devzone-ti.vercel.app/",
+    },
+    {
+      title: "itZmyLink- One Page many Links",
+      description:
+        "itZmyLink is a simple platform where user can create a personalized page to showcase all your social media profiles in one place. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/itZmyLink",
+    },
+    {
+      title: "GitEstimate- Github estimate worth generator",
+      description:
+        "GitEstimate is a simple fun tool where user can generate their github estimate worth card just by entering their github username. ",
+      tags: ["Nextjs", "Typescript", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/GitEstimate",
+    },
+    {
+      title: "Mixcn-ui- Reusable components for Nextjs",
+      description:
+        "This is Nextjs app with a Collection of Nextjs Components - (Currently under Devlelopment) ",
+      tags: ["Nextjs", "Shadcnui", "Npx", "Library"],
+      link: "https://mixcn-ui.vercel.app",
+    },
+    {
+      title: "Rupeespot- All deals Product in one place",
+      description:
+        "This is a free tool to check price history charts for millions of products for popular Indian stores such as Amazon and Flipkart. ",
+      tags: ["Nextjs", "Freelancing", "MySQL"],
+      link: "https://rupeespot.com/",
+    },
+    {
+      title: "Formcraft- Build Type-Safe Forms with Zero Effort",
+      description:
+        "Create production-ready forms with Zod validation, React Hook Form, and TypeScript. Design, customize, and deploy in minutes.",
+      tags: ["Nextjs", "Shadcnui", "Zod", "React Hook Form "],
+      link: "https://formcraft-ti.vercel.app/",
+    },
+    {
+      title: "KryptoKapital- Investing in Cryptocurreny",
+      description:
+        "This is a platform for learning about cryptocurrency with many tools and festures. It is very big project. ",
+      tags: ["Nextjs", "Freelancing", "Supabase"],
+      link: "https://pro.kryptokapital.info/",
+    },
+    {
+      title: "FriendZ - A social media Platform",
+      description:
+        "FriendZ is a social media app made with modern tech stacks such as redis, Authjs etc. A user can Create, delete, like, comment Post. ",
+      tags: ["Redis", "Authjs", "Typescript", "Nextjs"],
+      link: "https://github.com/taqui-786/project-friendz",
+    },
+    {
+      title: "ChatApp- Simple instagram clone",
+      description:
+        "ChatApp is similar to instagram, you can create, edit, delete, like,comment, post and  Follow, unfollow users",
+      tags: ["Reactjs", "Javascript"],
+      link: "https://github.com/taqui-786/chatapp-api",
+    },
+    {
+      title: "Devletter - A Newsletter for devs",
+      description:
+        "Devletter ia a newsletter subscribing single page app made with Nextjs and Tailwindcss.",
+      tags: ["Nextjs", "Tailwindcss"],
+      link: "https://github.com/taqui-786/Devletter",
+    },
+    {
+      title: "CrouMaker - A Crousal Maker App",
+      description:
+        "Crousal Maker is a tool with in-built crousals templates edit and download it in any format.",
+      tags: ["Nextjs", "jsPDF", "html2canvas", "Shadcn Ui"],
+      link: "https://github.com/taqui-786/crousal-maker",
+    }, */
+  ],
+
+  // More Links Information
+  moreLinks: [
+    {
+      title: "Dev.to",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Dev.to ",
+      link: "https://dev.to/random_ti",
+    },
+    {
+      title: "Hashnode",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Hashnode",
+      link: "https://mdtaquiimam.hashnode.dev/",
+    },
+    {
+      title: "Medium",
+      description:
+        "I write blogs on web development, trending tech stacks or javascript guide or tips in Medium",
+      link: "https://medium.com/@mdtaqui.jhar",
+    },
+    {
+      title: "Daily.Dev",
+      description:
+        "I am also the member of Daily Dev squads and i also upload post on some squads.",
+      link: "https://app.daily.dev/taqui_786",
+    },
+    {
+      title: "Gumroad",
+      description:
+        "I also sell digital products on gumroad like Notion Templates and web projects etc.",
+      link: "https://mdtaquijhar.gumroad.com/",
+    },
+    {
+      title: "Buy Me a coffee",
+      description:
+        "Supports my content quality, new topics, and project creation.",
+      link: "https://www.buymeacoffee.com/taquidevloper",
+    },
+  ],
+
+  // API Keys (should be in .env but referenced here)
+  apiKeys: {
+    resendApiKey: "YOUR_RESEND_API_KEY", // This should be loaded from environment variables in a real app
+  },
+};
